7f55db815a9941fc0b1081395f5e11174c4903f0
Kushagra Gupta <kushagragupta853@gmail.com>
2025-03-06 10:52:29 +0530

    [Kushagra] Update. (Employee Wager) Storing Daily Wage with Total Wage program in UC8

--- employee-wager.js
+++ employee-wager.js
@@ -38,8 +38,9 @@ const MAX_HRS_IN_MONTH = 160;
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
 
-// UC6: Array
+// UC6: Array and 7 Maps
 let empDailyWageArr = new Array();
+let empDailyWageMap = new Map();
 
 function calcDailyWage(empHrs) {
     return empHrs * WAGE_PER_HOUR;
@@ -51,7 +52,15 @@ while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
     let empHrs = getWorkingHours(empCheck);
     totalEmpHrs += empHrs;
     empDailyWageArr.push(calcDailyWage(empHrs));
+    empDailyWageMap.set(totalWorkingDays, calcDailyWage(empHrs));
 }
+
+console.log(empDailyWageMap);
+function totalWages(totalWage, dailyWage) {
+    return totalWage + dailyWage
+}
+
+console.log("UC7A Emp Wage Map totalHrs: " + Array.from(empDailyWageMap.values()).reduce(totalWages, 0));
 
 // Array Helper Functions
 // UC-7A: Calculate total wage using Array forEach traversal or reduce method
